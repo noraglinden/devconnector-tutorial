eb9f2eb24748786bb51cd11b528e62d9fc1aa81f
Nora <nora.harris@daugherty.com>
2019-09-28 15:08:52 -0500

    logic for jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,13 +2,16 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const { check, validationResult } = require('express-validator')
+const config = require('config')
 
 const User = require('../../models/User')
 
 // @route POST api/users
 // @desc Register user
 // @access Public
+// @return jwt for reigstered user or errors
 router.post(
   '/',
   [
@@ -56,8 +59,23 @@ router.post(
       user.password = await bcrypt.hash(password, salt)
 
       await user.save()
-      //return jsonwebtoken
-      res.send('User registered.')
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      //should be 3600 in production
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000000 },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
     } catch (err) {
       console.error(err.message)
       res.status(500).send('Server error')
