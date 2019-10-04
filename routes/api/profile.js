5c60602c6b77b4d7f395ae12e5012df458545edc
Nora <nora.harris@daugherty.com>
2019-10-04 10:31:37 -0500

    added delete route for user and profile todo delete related user posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -146,4 +146,24 @@ router.get('/user/:userId', async (req, res) => {
   }
 })
 
+// @route DELETE api/profile
+// @desc Delete profile, user, & posts
+// @access Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    //todo remove user posts
+
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id })
+
+    //remove user
+    await User.findOneAndRemove({ _id: req.user.id })
+
+    res.json({ msg: 'User removed' })
+  } catch (err) {
+    console.log(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
 module.exports = router
