be17a9b6e1ed8a33464e8c8f50a114bfe08217cf
Nora <nora.harris@daugherty.com>
2019-10-04 10:24:26 -0500

    add get all profiles and get profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -109,4 +109,41 @@ router.post(
   }
 )
 
+// @route GET api/profile
+// @desc Get all profiles
+// @access Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+    res.json(profiles)
+  } catch (err) {
+    console.log(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route GET api/profile/user/:userId
+// @desc Get profile by userId
+// @access Public
+router.get('/user/:userId', async (req, res) => {
+  //todo add express-validator on param
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.userId
+    }).populate('user', ['name', 'avatar'])
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' })
+    }
+
+    res.json(profile)
+  } catch (err) {
+    console.log(err.message)
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' })
+    }
+    res.status(500).send('Server Error')
+  }
+})
+
 module.exports = router
