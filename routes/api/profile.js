2dd33ce73a8b0313ac632e0d290a824dfe6f85e7
Nora <nora.harris@daugherty.com>
2019-10-04 09:25:32 -0500

    route for get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
 
-// @route GET api/profiles
-// @desc Test route
-// @access Public
-router.get('/', (req, res) => {
-  res.send('Profile route')
+const User = require('../../models/User')
+const Profile = require('../../models/Profile')
+
+// @route GET api/profiles/me
+// @desc Get current user profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    )
+
+    //todo string interpolation
+    if (!profile) {
+      return res.status(401).json({ msg: 'No profile found for this user.' })
+    }
+
+    res.json(profile)
+  } catch (err) {
+    console.log(err.message)
+    res.status(500).send('Server Error')
+  }
 })
 
 module.exports = router
