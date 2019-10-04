e5a9343f20a37a45efe0dcaffb6b3c1663e583d1
Nora <nora.harris@daugherty.com>
2019-10-04 13:30:24 -0500

    added add and delete comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -161,10 +161,99 @@ router.put('/unlike/:postId', auth, async (req, res) => {
       .indexOf(req.user.id)
 
     post.likes.splice(removeIndex, 1)
-    post.save()
+    await post.save()
     res.json(post.likes)
   } catch (err) {
     console.log(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route PUT api/posts/comment/:postId
+// @desc Comment on a post by Id
+// @access Private
+router.put(
+  '/comment/:postId',
+  [
+    auth,
+    [
+      check('text', 'Comment text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ msg: errors.array() })
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password')
+      const post = await Post.findById(req.params.postId)
+
+      if (!post) {
+        return res.status(404).json({ msg: 'No post found for this id' })
+      }
+
+      const newComment = {
+        user: req.user.id,
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar
+      }
+
+      post.comments.unshift(newComment)
+      await post.save()
+      res.json(post.comments)
+    } catch (error) {
+      console.log(error.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
+
+// @route DELETE api/posts/comment/:postId/:commentId
+// @desc Delete comment by Id
+// @access Private
+router.delete('/comment/:postId/:commentId', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId)
+
+    if (!post) {
+      return res.status(404).json({ msg: 'No post found for this id' })
+    }
+
+    //Pull out comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.commentId
+    )
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'No comment found for this id' })
+    }
+
+    //Check comment belongs to the user before deleteing
+    if (comment.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: 'User not authorized to delete comment' })
+    }
+
+    const removeIndex = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id)
+
+    post.comments.splice(removeIndex, 1)
+    await post.save()
+
+    res.json({ msg: 'Comment successfully deleted' })
+  } catch (err) {
+    console.log(err.message)
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'No post found for this id' })
+    }
     res.status(500).send('Server Error')
   }
 })
