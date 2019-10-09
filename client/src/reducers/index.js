8d4681e055979c78aab0683aa6125eaa99a77c79
Nora <nora.harris@daugherty.com>
2019-10-09 14:18:54 -0500

    fixed reducer store to have auth as state

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
 import alert from './alert'
-import register from './auth'
+import auth from './auth'
 
-export default combineReducers({ alert, register })
+export default combineReducers({ alert, auth })
