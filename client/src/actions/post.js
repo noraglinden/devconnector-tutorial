3efe01f32aa3a45f3eec3c8e6e77629f3c8f893f
Nora <nora.harris@daugherty.com>
2019-10-12 11:23:40 -0500

    created add post action and handled in reducer

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
 import { setAlert } from './alert'
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types'
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST
+} from './types'
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -50,6 +56,25 @@ export const deletePost = postId => async dispatch => {
     const res = await axios.delete(`/api/posts/${postId}`)
     dispatch({ type: DELETE_POST, payload: postId })
     dispatch(setAlert('Post deleted'))
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
+}
+
+// Add Post
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  }
+  try {
+    const res = await axios.post('/api/posts', formData, config)
+    dispatch({ type: ADD_POST, payload: res.data })
+    dispatch(setAlert('Post created', 'success'))
   } catch (err) {
     dispatch({
       type: POST_ERROR,
