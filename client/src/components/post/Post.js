2e137d3d10651611f365f039097060915b42d9bc
Nora <nora.harris@daugherty.com>
2019-10-12 16:56:37 -0500

    added comment form and implemented in Post

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import Spinner from '../layout/Spinner'
 import PostItem from '../posts/PostItem'
 import { getPost } from '../../actions/post'
 import { Link } from 'react-router-dom'
+import CommentForm from './CommentForm'
 
 const Post = ({ post: { post, loading }, getPost, match }) => {
   useEffect(() => {
@@ -19,6 +20,7 @@ const Post = ({ post: { post, loading }, getPost, match }) => {
         Back to Posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
     </Fragment>
   )
 }
