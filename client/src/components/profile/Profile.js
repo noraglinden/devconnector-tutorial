7e2c9d09b91e11e19613a98bd8878eebf118667b
Nora <nora.harris@daugherty.com>
2019-10-12 09:54:43 -0500

    component to see all github repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
 import ProfileExperience from './ProfileExperience'
 import ProfileEducation from './ProfileEducation'
+import ProfileGithub from './ProfileGithub'
 import { Link } from 'react-router-dom'
 
 const Profile = ({
@@ -39,7 +40,7 @@ const Profile = ({
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
             <div className='profile-exp bg-white p-2'>
-              <h2 class='text-primary'>Experience</h2>
+              <h2 className='text-primary'>Experience</h2>
               {profile.experience.length > 0 ? (
                 <Fragment>
                   {profile.experience.map(exp => (
@@ -51,7 +52,7 @@ const Profile = ({
               )}
             </div>
             <div className='profile-edu bg-white p-2'>
-              <h2 class='text-primary'>Education</h2>
+              <h2 className='text-primary'>Education</h2>
               {profile.education.length > 0 ? (
                 <Fragment>
                   {profile.education.map(edu => (
@@ -62,6 +63,10 @@ const Profile = ({
                 <h4>No education credentials</h4>
               )}
             </div>
+
+            {profile.githubUsername && (
+              <ProfileGithub username={profile.githubUsername} />
+            )}
           </div>
         </Fragment>
       )}
