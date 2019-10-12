d46b1260aa02f310671863fad0822c88bfd448a2
Nora <nora.harris@daugherty.com>
2019-10-12 13:36:16 -0500

    added get post router

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import PrivateRoute from './components/routing/PrivateRoute'
 import Profiles from './components/profiles/Profiles'
 import Profile from './components/profile/Profile'
 import Posts from './components/posts/Posts'
+import Post from './components/post/Post'
 
 // Redux
 import { Provider } from 'react-redux'
@@ -68,6 +69,7 @@ const App = () => {
                 component={AddEducation}
               />
               <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/post/:id' component={Post} />
             </Switch>
           </section>
         </Fragment>
